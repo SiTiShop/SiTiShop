bb08dbddd950ff59977aa8a37f89080926037b44
minkuan-buu <minhquandoanngoc@gmail.com>
2023-07-07 13:48:48 +0700

    main

---
+++ SiTiShop.Data/Entities/tblSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiTiShop.Data.Entities
+{
+    public partial class tblSize
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Status { get; set; }
+
+    }
+}
