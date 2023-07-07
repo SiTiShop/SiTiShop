29f59d69d6ba45b66071880fc195e69cfde92d5e
minkuan-buu <minhquandoanngoc@gmail.com>
2023-07-07 12:21:47 +0700

    main

---
+++ SiTiShop.Data/Entities/tblUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiTiShop.Data.Entities
+{
+    internal class tblUser
+    {
+        public Guid Id { get; set; }
+
+        public string UserName { get; set; } = null;
+
+        public string Password { get; set; } = null;
+
+        public string? Fullname { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
